ee83429361c07e0ef47dbf5b8339d6c326694679
Monika Kaploniak <monicakaploniak@gmail.com>
2025-06-04 11:03:32 +0100

    fix reference to processed bucket name

--- terraform/s3_processed_bucket.tf
+++ terraform/s3_processed_bucket.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "processed_bucket" {
-  bucket = var.ingestion_bucket_name
+  bucket = var.processed_bucket_name
   tags = {
     Name        = "Totesys Processed Bucket"
     Environment = "Dev"
