6ab9e471996068701be2ce39b1f3c2e42c07aada
Monika Kaploniak <monicakaploniak@gmail.com>
2025-06-06 08:40:59 +0100

    update dim staff table

--- sql/schema/create_dim_staff.sql
+++ sql/schema/create_dim_staff.sql
@@ -1,4 +1,4 @@
-CREATE TABLE dim_staff (
+CREATE TABLE IF NOT EXISTS dim_staff (
     staff_id INT PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
@@ -6,3 +6,24 @@ CREATE TABLE dim_staff (
     location VARCHAR(100) NOT NULL,
     email_address VARCHAR(100) NOT NULL
 );
+
+INSERT INTO dim_staff (
+    staff_id,
+    first_name,
+    last_name,
+    department_name,
+    location,
+    email_address
+)
+SELECT DISTINCT
+    staff.staff_id,
+    staff.first_name,
+    staff.last_name,
+    department.department_name,
+    department.location,
+    staff.email_address
+FROM staff
+JOIN department ON staff.department_id = department.department_id;
+
+-- 1. Create and populate dimension tables
+-- 2. Create and populate fact table
--- sql/transform/transform_dim_staff.sql
+++ sql/transform/transform_dim_staff.sql
@@ -1,12 +1,13 @@
+-- might be unnecessary - check
 SELECT 
     staff.staff_id,
     staff.first_name,
     staff.last_name,
     department.department_name,
-    -- staff.location,
+    department.location,
     staff.email_address
 FROM
     staff
 LEFT JOIN
     department
-ON staff.department_id = department.department_id
+ON staff.department_id = department.department_id;
