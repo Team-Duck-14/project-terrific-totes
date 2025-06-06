6c660b6a0ac66440e6fbb3e415f19c396980b5c8
Monika Kaploniak <monicakaploniak@gmail.com>
2025-06-06 09:32:38 +0100

    populate dim_location table

--- sql/schema/create_dim_location.sql
+++ sql/schema/create_dim_location.sql
@@ -1,4 +1,4 @@
-CREATE TABLE dim_location (
+CREATE TABLE IF NOT EXISTS dim_location (
     location_id INT PRIMARY KEY,
     address_line_1 VARCHAR(100) NOT NULL,
     address_line_2 VARCHAR(100),
@@ -8,3 +8,24 @@ CREATE TABLE dim_location (
     country VARCHAR(100) NOT NULL,
     phone VARCHAR(100) NOT NULL
 );
+
+INSERT INTO dim_location (
+    location_id,
+    address_line_1,
+    address_line_2,
+    district,
+    city,
+    postal_code,
+    country,
+    phone
+)
+SELECT DISTINCT
+    address_id AS location_id,
+    address_line_1,
+    address_line_2,
+    district,
+    city,
+    postal_code,
+    country,
+    phone
+FROM address;
