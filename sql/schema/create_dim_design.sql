e30e4867b0c5b2b38cf7257fee3c9b752389f45f
Monika Kaploniak <monicakaploniak@gmail.com>
2025-06-06 09:14:10 +0100

    populate dim_design table

--- sql/schema/create_dim_design.sql
+++ sql/schema/create_dim_design.sql
@@ -1,6 +1,19 @@
-CREATE TABLE dim_design (
+CREATE TABLE IF NOT EXISTS dim_design (
     design_id INT PRIMARY KEY,
     design_name VARCHAR(100) NOT NULL,
     file_location VARCHAR(100) NOT NULL,
     file_name VARCHAR(100) NOT NULL
 );
+
+INSERT INTO dim_design (
+    design_id,
+    design_name,
+    file_location,
+    file_name
+)
+SELECT DISTINCT
+    design_id,
+    design_name,
+    file_location,
+    file_name
+FROM design;
