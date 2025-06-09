e5c5734728e1ad0e8cd0fc0620182840a904d235
Timmy <timmonaghan4@gmail.com>
2025-06-09 10:30:21 +0100

    add tested totesys data scan

--- test/test_ingestion_totesys_new_data_scan.py
+++ test/test_ingestion_totesys_new_data_scan.py
@@ -7,6 +7,7 @@ import pg8000
 import os
 import datetime
 import boto3
+import pandas as pd
 from moto import mock_aws
 from dotenv import load_dotenv
 
@@ -26,13 +27,13 @@ def s3_mock(mock_aws_credentials):
         yield
 
 # Uncomment to test
-# conn = pg8000.native.Connection(
-#         user = os.environ['TOTESYS_USER'],
-#         password = os.environ['TOTESYS_PASSWORD'],
-#         host = os.environ['TOTESYS_HOST'],
-#         database = os.environ['TOTESYS_DATABASE'],
-#         port = os.environ['TOTESYS_PORT']
-#         )
+conn =  pg8000.native.Connection(
+        user = os.environ['TOTESYS_USER'],
+        password = os.environ['TOTESYS_PASSWORD'],
+        host = os.environ['TOTESYS_HOST'],
+        database = os.environ['TOTESYS_DATABASE'],
+        port = os.environ['TOTESYS_PORT']
+        )
 
 @pytest.mark.skip(reason="passed, but now fails because function was build up with TDD")
 def test_totesys_gets_data_from_totesys(conn, s3_mock):
@@ -45,27 +46,31 @@ def test_totesys_gets_data_from_totesys(conn, s3_mock):
     assert len(response[0]) > 0
 
 @pytest.mark.skip(reason="passed, but now fails because function was build up with TDD")
-def test_totesys_get_only_new_data(conn, s3_mock):
+def test_totesys_get_only_new_data(s3_mock):
     client = boto3.client("s3", region_name="eu-west-2")
+    client.create_bucket(Bucket = "project-totesys-ingestion-bucket",
+                         CreateBucketConfiguration={
+                            'LocationConstraint': 'eu-west-2'})
     response = look_for_totesys_updates(conn, client)
     demo_timestamp = datetime.datetime(2000,11,3,14,20,52,186)
 
     # check only data that is fresher than demo_timestamp has been ingested
     for table in response:
+        print(table)
         for i in table.loc[:,"created_at"]:
             assert i >= demo_timestamp
         for i in table.loc[:,"last_updated"]:
             assert i >= demo_timestamp
 
 @pytest.mark.skip(reason="github actions machine has no access to ToteSys credentials")         
-def test_totesys_puts_new_data_in_s3_bucket(conn, s3_mock):
+def test_totesys_puts_new_data_in_s3_bucket(s3_mock):
     client = boto3.client("s3", region_name="eu-west-2")
-    client.create_bucket(Bucket = "totesys-ingestion-bucket",
+    client.create_bucket(Bucket = "project-totesys-ingestion-bucket",
                          CreateBucketConfiguration={
     'LocationConstraint': 'eu-west-2'})
 
     response = look_for_totesys_updates(conn, client)
-    assert response == {"Status Code": 200, "body": f"Uploaded 11 tables to S3 totesys-ingestion-bucket"}
+    assert response == {"Status Code": 200, "body": f"Uploaded new or updated values for 11 tables to S3 project-totesys-ingestion-bucket"}
 
 @pytest.mark.skip(reason="passed, but now fails because we broke the function in order to test")
 def test_totesys_scan_returns_error(conn, s3_mock):
